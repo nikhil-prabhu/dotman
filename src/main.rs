c05fa5bf627b4de1be4222aa8bdf1e8db026c0ab
Nikhil Prabhu <nikhilprabhu00@gmail.com>
2021-06-12 09:59:09 +0000

    Initialize CLI app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use structopt::StructOpt;
+
+/// > Help message goes here. <
+#[derive(StructOpt)]
+struct Flags {
+    // Will be populated as functionality is added.
+}
+
 fn main() {
-    println!("Hello, world!");
+    // We won't use this variable yet. We just initialize the CLI app for now.
+    let _flags = Flags::from_args();
+
+    println!("A command line utility for managing *nix dotfiles; written in Rust.");
 }
