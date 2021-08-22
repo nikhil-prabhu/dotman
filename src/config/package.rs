3f5ec005f8bf9f04a47340f95392a1c6f6c4986e
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2021-08-22 07:25:12 +0000

    Add experimental support for installing packages

--- src/config/package.rs
+++ src/config/package.rs
@@ -1,14 +1,36 @@
-//! Module for performing package related tasks.
+//! Module for performing package management tasks.
+//! **WARNING: This module is highly experimental and should be used with caution.**
 extern crate serde_json;
 
 use crate::logger::Logger;
+use packagekit::PackageKit;
 use std::io::Write;
 
+// TODO: add support for specifying package versions.
 /// Installs a list of packages onto the system.
-pub fn install<W>(_args: &serde_json::Value, logger: &mut Logger<W>)
+pub fn install<W>(args: &serde_json::Value, logger: &mut Logger<W>)
 where
     W: Write,
 {
-    // TODO: implement.
-    logger.warn("Nothing to do.");
+    if let Some(packages) = &args.as_array() {
+        let pk = PackageKit::new();
+
+        // TODO: For now we just unwrap on error. Proper error handling should be added.
+        for pkg in packages.iter() {
+            if let Some(s) = pkg.as_str() {
+                // NOTE: Only the first matching package will be installed.
+                // ? Maybe this isn't the best thing to do?
+                let results = pk.search_package(s).unwrap();
+
+                logger.info(&format!("Installing package: {}", results[0].id()));
+                pk.install(&results[0]).unwrap();
+            } else {
+                // ? Should probably handle invalid args in a better way.
+                logger.fatal("Invalid package name in list of packages.");
+            }
+        }
+        logger.success("Done.");
+    } else {
+        logger.warn("Nothing to do.");
+    }
 }
